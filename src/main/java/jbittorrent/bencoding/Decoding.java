74b07cb60867324cbc6dc00ce5eabd7a2ee9031d
Adam Michael <ajmichael0@gmail.com>
2015-04-29 23:59:10 -0400

    decoding for lists and dictionaries with tests

--- src/main/java/jbittorrent/bencoding/Decoding.java
+++ src/main/java/jbittorrent/bencoding/Decoding.java
@@ -1,10 +1,15 @@
 package jbittorrent.bencoding;
 
+import java.util.List;
+import java.util.Map;
+
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 public final class Decoding {
 
-  public static Intermediary<String> readString(String encoded) {
+  static Intermediary<String> readString(String encoded) {
     Preconditions.checkArgument(Character.isDigit(encoded.charAt(0)));
     int splitIndex = encoded.indexOf(':');
     int length = Integer.parseInt(encoded.substring(0, splitIndex));
@@ -14,7 +19,7 @@ public final class Decoding {
         valueAndRemainder.substring(length));
   }
 
-  public static Intermediary<Integer> readInteger(String encoded) {
+  static Intermediary<Integer> readInteger(String encoded) {
     Preconditions.checkArgument(encoded.charAt(0) == 'i');
     int splitIndex = encoded.indexOf('e');
     return Intermediary.of(
@@ -22,7 +27,47 @@ public final class Decoding {
         encoded.substring(splitIndex + 1));
   }
 
-  public static class Intermediary<T> {
+  static Intermediary<List<Object>> readList(String encoded) {
+    Preconditions.checkArgument(encoded.charAt(0) == 'l');
+    encoded = encoded.substring(1);
+    List<Object> list = Lists.newLinkedList();
+    while (encoded.charAt(0) != 'e') {
+      Intermediary<? extends Object> intermediary = readObject(encoded);
+      list.add(intermediary.decodedValue);
+      encoded = intermediary.encodedRemainder;
+    }
+    return Intermediary.of(list, encoded.substring(1));
+  }
+
+  static Intermediary<Map<String, Object>> readDictionary(String encoded) {
+    Preconditions.checkArgument(encoded.charAt(0) == 'd');
+    encoded = encoded.substring(1);
+    Map<String, Object> dictionary = Maps.newHashMap();
+    while (encoded.charAt(0) != 'e') {
+      Intermediary<String> intermediaryKey = readString(encoded);
+      Intermediary<? extends Object> intermediaryValue =
+          readObject(intermediaryKey.encodedRemainder);
+      dictionary.put(intermediaryKey.decodedValue, intermediaryValue.decodedValue);
+      encoded = intermediaryValue.encodedRemainder;
+    }
+    return Intermediary.of(dictionary, encoded.substring(1));
+  }
+
+  public static Intermediary<? extends Object> readObject(String encoded) {
+    if (Character.isDigit(encoded.charAt(0))) {
+      return readString(encoded);
+    } else if (encoded.charAt(0) == 'i') {
+      return readInteger(encoded);
+    } else if (encoded.charAt(0) == 'l') {
+      return readList(encoded);
+    } else if (encoded.charAt(0) == 'd') {
+      return readDictionary(encoded);
+    } else {
+      throw new InvalidEncodingException(encoded);
+    }
+  }
+
+  static class Intermediary<T> {
     final T decodedValue;
     final String encodedRemainder;
 
@@ -37,4 +82,10 @@ public final class Decoding {
       return new Intermediary<T>(decodedValue, encodedRemainder);
     }
   }
+  
+  static class InvalidEncodingException extends RuntimeException {
+    public InvalidEncodingException(String invalidString) {
+      super(invalidString);
+    }
+  }; 
 }
--- src/test/java/jbittorrent/bencoding/DecodingTest.java
+++ src/test/java/jbittorrent/bencoding/DecodingTest.java
@@ -1,37 +1,104 @@
 package jbittorrent.bencoding;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import jbittorrent.bencoding.Decoding.Intermediary;
+import jbittorrent.bencoding.Decoding.InvalidEncodingException;
 
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableSet;
+
 public class DecodingTest {
 
-  @Test public void testSuccessfulReadString() {
+  @Test
+  public void testSuccessfulReadString() {
     String encodedData = "4:spamd3:cow3:mooe";
     Intermediary<String> intermediary = Decoding.readString(encodedData);
     assertEquals("spam", intermediary.decodedValue);
     assertEquals("d3:cow3:mooe", intermediary.encodedRemainder);
   }
-  
+
   @Test(expected = IllegalArgumentException.class)
   public void testInvalidReadString() {
     String encodedData = "i3549e4:spamd3:cow3:mooe";
     Decoding.readString(encodedData);
   }
-  
+
   @Test
   public void testSuccessfulReadInteger() {
     String encodedData = "i3549e4:spamd3:cow3:mooe";
     Intermediary<Integer> intermediary = Decoding.readInteger(encodedData);
     assertEquals(new Integer(3549), intermediary.decodedValue);
-    assertEquals("4:spamd3:cow3:mooe", intermediary.encodedRemainder);    
+    assertEquals("4:spamd3:cow3:mooe", intermediary.encodedRemainder);
   }
-  
+
   @Test(expected = IllegalArgumentException.class)
   public void testInvalidReadInteger() {
     String encodedData = "4:spamd3:cow3:mooe";
     Decoding.readInteger(encodedData);
   }
+
+  @Test
+  public void testSuccessfulReadList() {
+    String encodedData = "l4:spam4:eggsei3546e";
+    Intermediary<List<Object>> intermediary = Decoding.readList(encodedData);
+    assertEquals("i3546e", intermediary.encodedRemainder);
+    List<Object> list = intermediary.decodedValue;
+    assertEquals(2, list.size());
+    assertTrue(list.get(0) instanceof String);
+    assertEquals("spam", list.get(0));
+    assertTrue(list.get(1) instanceof String);
+    assertEquals("eggs", list.get(1));
+  }
   
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidReadList() {
+    String encodedData = "4:spamd3:cow3:mooe";
+    Decoding.readList(encodedData);
+  }
+  
+  @Test
+  public void testSuccessfulReadDictionary() {
+    String encodedData = "d3:cow3:moo4:spami45eei123e";
+    Intermediary<Map<String, Object>> intermediary = Decoding.readDictionary(encodedData);
+    assertEquals("i123e", intermediary.encodedRemainder);
+    Map<String, Object> dictionary = intermediary.decodedValue;
+    assertEquals(2, dictionary.size());
+    Set<String> expectedKeys = ImmutableSet.of("cow", "spam");
+    assertEquals(expectedKeys, dictionary.keySet());
+    assertEquals("moo", dictionary.get("cow"));
+    assertEquals(45, dictionary.get("spam"));
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidReadDictionary() {
+    String encodedData = "l4:spam4:eggsei3546e";
+    Decoding.readDictionary(encodedData);
+  }
+  
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testSuccessfulReadObject() {
+    String encodedData = "l4:spam4:eggsei3546e";
+    Intermediary<? extends Object> intermediary = Decoding.readObject(encodedData);
+    assertEquals("i3546e", intermediary.encodedRemainder);
+    List<Object> list = (List<Object>) intermediary.decodedValue;
+    assertEquals(2, list.size());
+    assertTrue(list.get(0) instanceof String);
+    assertEquals("spam", list.get(0));
+    assertTrue(list.get(1) instanceof String);
+    assertEquals("eggs", list.get(1));
+  }
+  
+  @Test(expected = InvalidEncodingException.class)
+  public void testInvalidReadObject() {
+    String encodedData = "adam";
+    Decoding.readObject(encodedData);
+  }
 }
